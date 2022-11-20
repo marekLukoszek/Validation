77949a56e5678df13a3e2454a49cb602a49692e2
Marek Lukoszek <m.lukoszek@wide-open.pl>
2022-11-20 18:52:04 +0100

    Commit

---
+++ src/main/java/com/example/simplevalidation/PersonService.java
@@ -0,0 +1,47 @@
+package com.example.simplevalidation;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validator;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PersonService {
+
+    private final Set<Person> people = new HashSet<>();
+    private final Validator validator;
+
+    @Autowired
+    public PersonService(Validator validator) {
+        this.validator = validator;
+    }
+
+    public void addPerson(Person person) {
+        Set<ConstraintViolation<Person>> errors = validator.validate(person);
+        if(!errors.isEmpty()) {
+            System.out.println(">Obiekt nie może być dodany, lista błędów:");
+            errors.forEach(err ->
+                    System.out.printf(">>> %s %s (%s)\n",
+                            err.getPropertyPath(),
+                            err.getMessage(),
+                            err.getInvalidValue())
+            );
+        } else {
+            people.add(person);
+            System.out.println(">Obiekt został dodany");
+        }
+    }
+
+    @Override
+    public String toString() {
+        String result = "People size " + people.size() + "\n";
+        for (Person person : people) {
+            result = result + person + "\n";
+        }
+        return result;
+    }
+}
